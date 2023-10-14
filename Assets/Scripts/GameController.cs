0840105b6dede0a8d540de0e356cda75298838ee
black_hole <sdarksoul99@gmail.com>
2023-10-14 15:24:36 +0200

    game_controller_roomNavigation-commit
    Game Controller:
    -Manage room navigation and action
    logs display
    
    -Room navigation: contained informations about the current room

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
+    public Text displayText;
+
     [HideInInspector] RoomNavigation roomNavigation;
+
+    public List<string> logActions = new List<string>();
 
     private void Awake()
     {
         roomNavigation = this.GetComponent<RoomNavigation>();
     }
 
-    private void DisplayText()
+    private void Start()
+    {
+        DisplayRoomText();
+        DisplayLoggedText();
+    }
+
+    public void DisplayLoggedText()
+    {
+        string logAsText = string.Join("\n", logActions.ToArray());
+        displayText.text = logAsText;
+    }
+
+    public void DisplayRoomText()
     {
         string combinedText = roomNavigation.currentRoom.description + "\n";
+        LogStringWithReturn(combinedText);
+    }
+
+    public void LogStringWithReturn(string stringToAdd)
+    {
+        logActions.Add(stringToAdd + "\n");
     }
 }
